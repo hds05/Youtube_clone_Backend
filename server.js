effb6e3e424c8dd62bd1a309808224e68e7c5831
hds05 <duttsharmahimanshu96@gmail.com>
2026-04-30 10:58:06 +0530

    Add CORS middleware to enable cross-origin requests

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 import MongoDB from './config/db.js'
 import userRoutes from './routes/userRoutes.js'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use(express.json())
 
