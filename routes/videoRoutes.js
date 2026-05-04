55452422eb9669d00cb7a5bf4016e610ac1b20f3
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-04 12:07:24 +0530

    add uploadVideo, getMyVideos, and uploadComment routes to VideoRoutes

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getVideos, getVideoById, addVideo } from "../controllers/videoController.js";
+import { getVideos, getVideoById, addVideo, uploadVideo, getMyVideos, uploadComment } from "../controllers/videoController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/videos", getVideos);
 router.get("/video/:id", getVideoById);
 router.post("/videos", addVideo);
+router.post("/upload", protect, uploadVideo);
+router.get("/myvideos", protect, getMyVideos);
+router.post('/video/:id/uploadComment', protect, uploadComment)
 
 export default router;
