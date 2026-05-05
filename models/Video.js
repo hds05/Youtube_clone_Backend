77b22ce49c1a8338bdc2acf1f56ef3084aeb0895
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-05 23:37:02 +0530

    update video and comment schemas for improved structure

--- models/Video.js
+++ models/Video.js
@@ -1,44 +1,77 @@
 import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
-  commentId: String,
-  userId: String,
-  text: String,
-  timestamp: Date,
-});
-
-const videoSchema = new mongoose.Schema({
-  videoId: { type: String, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  category: { type: String, required: true },
-  thumbnailUrl: { type: String, required: true },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
-    required: true,
   },
-  uploader: String,
-  channelId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Channel",
-    required: true,
-  },
-  channelName: { type: String, required: true },
-  channelIcon: String,
-  views: String,
-  likes: String,
-  dislikes: String,
-
-  uploadDate: {
+  userName: String,
+  text: String,
+  timestamp: {
     type: Date,
     default: Date.now,
   },
-  subscribers: String,
-  time: String,
-  comments: [commentSchema],
-  time: String,
 });
+
+const videoSchema = new mongoose.Schema(
+  {
+    // videoId: { type: String},
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    thumbnailUrl: { type: String, required: true },
+
+    videoType: {
+      type: String,
+      enum: ["youtube", "upload"],
+      required: true,
+    },
+    videoUrl: {
+      type: String,
+      required: true,
+    },
+
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    uploader: String,
+    channelId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Channel",
+    },
+    channelName: { type: String, required: true },
+    channelIcon: String,
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+
+    uploadDate: {
+      type: Date,
+      default: Date.now,
+    },
+    subscribers: {
+      type: Number,
+      default: 0,
+    },
+    comments: [commentSchema],
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Video = mongoose.model("Video", videoSchema);
 
