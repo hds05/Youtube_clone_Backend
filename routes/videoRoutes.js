6873cf4447928ec83b06b40247a79d335553c10f
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-06 16:39:46 +0530

    create routes for edit and delete videos uploaded on channel

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,5 +1,16 @@
 import express from "express";
-import { getVideos, getVideoById, addVideo, uploadVideo, getMyVideos, uploadComment, editComment, deleteComment, deleteMyVideo } from "../controllers/videoController.js";
+import {
+  getVideos,
+  getVideoById,
+  addVideo,
+  uploadVideo,
+  getMyVideos,
+  uploadComment,
+  editComment,
+  deleteComment,
+  deleteMyVideo,
+  editMyVideo,
+} from "../controllers/videoController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import upload from "../middleware/upload.js";
 
@@ -8,11 +19,14 @@ const router = express.Router();
 router.get("/videos", getVideos);
 router.get("/video/:id", getVideoById);
 router.post("/videos", addVideo);
-router.post("/upload", protect, upload.single("videoFile"), uploadVideo)
+router.post("/upload", protect, upload.single("videoFile"), uploadVideo);
 router.get("/myvideos", protect, getMyVideos);
 
-router.post('/video/:id/uploadComment', protect, uploadComment)
-router.put('/video/:id/editComment', protect, editComment);
-router.delete('/video/:id/deleteComment', protect, deleteComment);
+router.post("/video/:id/uploadComment", protect, uploadComment);
+router.put("/video/:id/editComment", protect, editComment);
+router.delete("/video/:id/deleteComment", protect, deleteComment);
+
+router.put("/video/:id/edit", protect, editMyVideo);
+router.delete("/video/:id", protect, deleteMyVideo);
 
 export default router;
