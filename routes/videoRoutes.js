7d4149b61b9833e5887677a59af8cdd26a1e54ac
hds05 <duttsharmahimanshu96@gmail.com>
2026-05-06 14:33:46 +0530

    update videoRoutes to include edit and delete comment functionality

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,14 +1,18 @@
 import express from "express";
-import { getVideos, getVideoById, addVideo, uploadVideo, getMyVideos, uploadComment } from "../controllers/videoController.js";
+import { getVideos, getVideoById, addVideo, uploadVideo, getMyVideos, uploadComment, editComment, deleteComment, deleteMyVideo } from "../controllers/videoController.js";
 import { protect } from "../middleware/authMiddleware.js";
+import upload from "../middleware/upload.js";
 
 const router = express.Router();
 
 router.get("/videos", getVideos);
 router.get("/video/:id", getVideoById);
 router.post("/videos", addVideo);
-router.post("/upload", protect, uploadVideo);
+router.post("/upload", protect, upload.single("videoFile"), uploadVideo)
 router.get("/myvideos", protect, getMyVideos);
+
 router.post('/video/:id/uploadComment', protect, uploadComment)
+router.put('/video/:id/editComment', protect, editComment);
+router.delete('/video/:id/deleteComment', protect, deleteComment);
 
 export default router;
